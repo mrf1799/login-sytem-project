0549245ed3328438fb2e338575a1a2349a6c381d
MD RAIHAN FIRDAUS <firdausraihan369@gmail.com>
2022-08-24 11:24:58 +0530

    first commit

---
+++ app.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+string directoryPath = "C:\\Users\\firda\\Desktop\\Projects\\login system\\";
+bool IsLoggedIn()
+{
+   string username, password, un, pw;
+   
+   cout << "Enter username: "; cin >> username;
+   cout << "Enter password: "; cin >> password;
+   
+   ifstream read (directoryPath + username + ".txt");
+   getline (read,un);
+   getline (read, pw);
+   
+   if (un == username && pw == password)
+   {
+       return true;
+   }
+   else
+   {
+       return false;
+   }
+}
+
+int main()
+{
+    int choice;
+    
+    cout << "1: Register\n2: Login\nYour choice : ";
+    cin >> choice;
+    if (choice == 1)
+    {
+        string username, password;
+        
+        cout << "select a username: "; cin >> username;
+        cout << "select a password: "; cin >> password;
+        
+        ofstream file;
+        file.open(directoryPath+username+".txt");
+        file << username << endl << password;
+        file.close();
+        
+        main();
+    }
+    else if (choice == 2)
+    {
+        bool status = IsLoggedIn();
+        
+        if (!status)
+        {
+            cout << "Login Failed!" << endl;
+            system("PAUSE");
+            return 0;
+        }
+        else
+        {
+            cout << "Succesfully logged in!" << endl;
+            system("PAUSE");
+            return 1;
+        }
+    }
+}
